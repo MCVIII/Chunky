b8fcdfbcaf83192bbe513b05e328dd5e54bb1a90
Dylan Araps <dylan.araps@gmail.com>
2021-10-30 10:07:41 +0300

    update

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -6,7 +6,7 @@ val shade: Configuration by configurations.creating
 
 dependencies {
     minecraft(group = "com.mojang", name = "minecraft", version = "21w43a")
-    mappings(group = "net.fabricmc", name = "yarn", version = "21w43a+build.2", classifier = "v2")
+    mappings(group = "net.fabricmc", name = "yarn", version = "21w43a+build.7", classifier = "v2")
     modImplementation(group = "net.fabricmc", name = "fabric-loader", version = "0.12.3")
     modImplementation(group = "net.fabricmc.fabric-api", name = "fabric-api", version = "0.41.2+1.18")
     implementation(project(":chunky-common"))
